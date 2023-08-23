496f2168e249ff5f01bb8cf1a4092580b4746a11
Алексей <118848145+GlazyrinAV@users.noreply.github.com>
2023-08-23 23:14:25 +0300

    - сформирована структура бд

--- ewm/ewm-service/src/main/resources/schema.sql
+++ ewm/ewm-service/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-drop table if exists users;
+drop table if exists event, users, category, state, event, locations;
 
 create table if not exists users
 (
@@ -8,8 +8,6 @@ create table if not exists users
     email varchar(255) not null unique
 );
 
-drop table if exists category;
-
 create table if not exists category
 (
     id   integer
@@ -17,16 +15,12 @@ create table if not exists category
     name varchar not null unique
 );
 
-drop table if exists state;
-
 create table if not exists state
 (
     id         integer
         primary key GENERATED ALWAYS AS IDENTITY,
     state_name integer not null unique
 );
-
-drop table if exists locations;
 
 create table if not exists locations
 (
@@ -36,25 +30,31 @@ create table if not exists locations
     lon double precision not null
 );
 
-drop table if exists event;
-
 create table if not exists event
 (
     id                 integer
         primary key GENERATED ALWAYS AS IDENTITY,
     title              varchar   not null,
     annotation         varchar   not null,
-    initiator_id       integer   not null,
+    initiator_id       integer   not null
+        constraint event_users_id_fk
+            references users,
     confirmed_requests integer,
-    category_id        integer   not null,
+    category_id        integer   not null
+        constraint event_category_id_fk
+            references category,
     created_on         timestamp,
     description        varchar,
     event_date         timestamp not null,
-    location_id        integer   not null,
+    location_id        integer   not null
+        constraint event_locations_id_fk
+            references locations,
     paid               boolean   not null,
     participant_limit  integer,
     published_on       timestamp,
     request_moderation boolean,
-    state_id           integer,
+    state_id           integer
+        constraint event_state_id_fk
+            references state,
     views              integer
 );
