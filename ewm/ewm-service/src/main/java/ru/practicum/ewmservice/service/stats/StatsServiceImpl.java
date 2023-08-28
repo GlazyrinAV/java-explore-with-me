1b7bce1a9860dd3cdb0ca6e80c3a7ba8072f7367
AVG <glazyrinav1986@yandex.ru>
2023-08-28 14:01:33 +0300

    - первая стабильная версия

--- ewm/ewm-service/src/main/java/ru/practicum/ewmservice/service/stats/StatsServiceImpl.java
+++ ewm/ewm-service/src/main/java/ru/practicum/ewmservice/service/stats/StatsServiceImpl.java
@@ -1,68 +1,48 @@
 package ru.practicum.ewmservice.service.stats;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
 
 @Service
 public class StatsServiceImpl implements StatsService {
 
-    public StatsServiceImpl(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
-    }
-
     @Value("${stats-client.url}")
     private String serverUrl;
 
-    private final RestTemplate restTemplate;
-
-    private HttpHeaders defaultHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-        return headers;
-    }
-
-    HttpEntity<String> entity = new HttpEntity<>(null, defaultHeaders());
-
     @Override
     public int getViews(int eventId) {
+
         String path = "/stats/" + eventId;
-        Integer hits = 0;
-        try {
-            URL url = new URL(serverUrl + path);
-            URLConnection conn = url.openConnection();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuffer sb = new StringBuffer();
 
-            int BUFFER_SIZE = 1024;
-            char[] buffer = new char[BUFFER_SIZE];
-            int charsRead = 0;
-            while ((charsRead = rd.read(buffer, 0, BUFFER_SIZE)) != -1) {
-                sb.append(buffer, 0, charsRead);
+        HttpGet httpget = new HttpGet(serverUrl + path);
+
+        ResponseHandler<String> responseHandler = response -> {
+            int status = response.getStatusLine().getStatusCode();
+            if (status >= 200 && status < 300) {
+                HttpEntity entity = response.getEntity();
+                return entity != null ? EntityUtils.toString(entity) : null;
+            } else {
+                throw new ClientProtocolException("Unexpected response status: " + status);
             }
+        };
 
-            ObjectMapper mapper = new ObjectMapper();
-            hits = mapper.readValue(sb.toString(), Integer.class);
-        } catch (JsonProcessingException e) {
-            System.out.println(e.getLocalizedMessage());
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            if (responseBody.isBlank()) {
+                return 0;
+            } else {
+                return Integer.parseInt(responseBody);
+            }
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if (hits != null) {
-            return hits;
-        } else {
             return 0;
         }
     }
